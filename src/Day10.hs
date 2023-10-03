79c30618dff43fa552a4226074998582ff6cc645
emandirola <esteban.mandirola@gmail.com>
2023-10-02 22:24:05 -0300

    Day10 part1 v2

--- src/Day10.hs
+++ src/Day10.hs
@@ -12,16 +12,16 @@ import GHC.Base ((<|>))
 
 preRead = lines
 
+third (_, _, t) = t
+
 day10part1 :: [String] -> String
-day10part1 = show . (\(_, _, s) -> s) . foldl' f (0, 1, 0)
+day10part1 = show . third . foldl' f (0, 1, 0)
     where
-        f' a b = traceShow (a, b, f'') f''
-            where f'' = f a b
         step :: Int -> Int -> Int -> Maybe Int
-        step cycle x strength = if shouldCount (cycle + 1) then Just $ strength + cycle * x else Nothing
+        step cycle x strength = if shouldCount (cycle + 1) then Just $ strength + (cycle + 1) * x else Nothing
         shouldCount cycle = (cycle - 20) `mod` 40 == 0
         f (cycle, x, strength) "noop" = (cycle + 1, x, fromMaybe strength $ step cycle x strength)
-        f (cycle, x, strength) instr = (cycle + 2, x + dx, fromMaybe strength $ step (cycle+1) (x+dx) strength <|> step cycle x strength)
+        f (cycle, x, strength) instr = (cycle + 2, x + dx, fromMaybe strength $ step (cycle+1) x strength <|> step cycle x strength)
             where dx = read $ dropWhile (not . (\x -> x == '-' || isNumber x)) instr
 
 day10part2 _ = "<expected part2>" --this way we can test only part1
