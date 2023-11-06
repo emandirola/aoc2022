995d4966758eda0394b55486fcbf3809f101db65
emandirola <esteban.mandirola@gmail.com>
2023-11-05 22:32:26 -0300

    Fix Day2

--- src/Day02.hs
+++ src/Day02.hs
@@ -2,10 +2,10 @@ module Day02 where
 import Data.List.Extra (elemIndices)
 
 play :: (Int, Int) -> Int
-play (them, us) = us + 1 + [0, 3, 6] !! ((them - us + 1) `mod` 3)
+play (them, us) = us + 1 + [0, 3, 6] !! ((us - them + 1) `mod` 3)
 
 play2 :: (Int, Int) -> Int
-play2 (them, outcome) = outcome + them + 1 `mod` 3 + 1
+play2 (them, outcome) = outcome * 3 + (outcome - 1 + them) `mod` 3 + 1
 
 day02 :: String -> (Int, Int)
 day02 = (,) <$> sum . map play <*> sum . map play2 <$> parse'
